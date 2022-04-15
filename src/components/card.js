ae42192face6b6c4199ecab397fe2184037394c8
unknown <macrowell@yahoo.com>
2022-04-15 11:52:11 -0400

    answered interview questions

--- src/components/card.js
+++ src/components/card.js
@@ -30,25 +30,6 @@ const Card = (article) => {
   })
 
   return cardDiv
-
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
 }
 
 const cardAppender = (selector) => {
@@ -80,21 +61,7 @@ const cardAppender = (selector) => {
   .catch(err => {
     console.error(err);
   })
-
-
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
 }
 
-axios.get(`http://localhost:5001/api/articles`)
-.then(res => {
-  console.log(res.data.articles)
-})
 
 export { Card, cardAppender }
