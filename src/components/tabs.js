217f40a0592ec7f06268316873be9c490243f4b0
unknown <macrowell@yahoo.com>
2022-04-15 10:52:35 -0400

    deleted commented out text from tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -10,21 +10,6 @@ const Tabs = (topics) => {
   })
   tabsDiv.classList.add("topics");
   return tabsDiv
-
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
 }
 
 
@@ -39,14 +24,6 @@ const tabsAppender = (selector) => {
   .catch((err) => {
     console.error(err);
   })
-
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
 }
 
 export { Tabs, tabsAppender }
